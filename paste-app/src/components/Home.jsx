7bf794f7490d450ce1e7dd610464f3adb246ed04
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-10-27 00:22:34 +0530

    Completed

--- paste-app/src/components/Home.jsx
+++ paste-app/src/components/Home.jsx
@@ -88,7 +88,7 @@ const Home = () => {
           <div
             className={`w-full rounded-t flex items-center justify-between gap-x-4 px-4 py-2 border-b border-[rgba(128,121,121,0.3)]`}
           >
-            <div className="w-full flex gap-x-[6px] items-center select-none group">
+            <div className="w-full flex gap-x-1.5 items-center select-none group">
               <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center  overflow-hidden bg-[rgb(255,95,87)]" />
 
               <div
